c121acc05adf68c392c7896a0fce2a319156cfe8
Liciacodes <udosenfelicia@gmail.com>
2025-06-11 16:37:51 +0100

    Added express js and routing

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const http = require("http");
 
-const routes = require('./routes')
+const express = require("express");
 
+const app = express();
 
-console.log(routes.someText); // Accessing the exported variable from routes.js
-const server = http.createServer(routes.handler)
+app.use('/', req, res, next => {
+    console.log("This always runs !");
+    next(); // Passes control to the next middleware
+})
 
-server.listen(3000);
+app.use('/add-products',(req, res, next) => {
+    console.log("Ending request processing...");
+    res.send("<h1>The 'Add Product Page'</h1>");
+ 
+  });
+
+app.use('/',(req, res, next) => {
+  console.log("Ending request processing...");
+  res.send("<h1>Goodbye from Express!</h1>");
+  next(); // Sends a response to the client
+});
+
+app.listen(3000);
