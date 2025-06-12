83fa52b20f71bd3db03b3d5fe0af6560acf020bd
Liciacodes <udosenfelicia@gmail.com>
2025-06-12 22:07:55 +0100

    parsed incoming request

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 
 const express = require("express");
-
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use('/', req, res, next => {
-    console.log("This always runs !");
-    next(); // Passes control to the next middleware
-})
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.use('/add-products',(req, res, next) => {
-    console.log("Ending request processing...");
-    res.send("<h1>The 'Add Product Page'</h1>");
+    res.send("<form action='/product' method='POST'><input type='text' name='title'></input><button type='submit'>Add Product</button></form>");
  
   });
 
+
+  app.use('/product',(req, res, next) => {
+    console.log(req.body)
+   res.redirect('/')
+   
+   
+  });
+
+  
 app.use('/',(req, res, next) => {
-  console.log("Ending request processing...");
   res.send("<h1>Goodbye from Express!</h1>");
-  next(); // Sends a response to the client
+ 
 });
 
 app.listen(3000);
