6cdb1b0612be9358528880993350718908044c36
Liciacodes <udosenfelicia@gmail.com>
2025-06-18 15:36:49 +0100

    learned how to use handlebars and ejs templating engines in nodejs

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+
+const path = require("path");
+
 const app = express();
 
-app.set('view engine', 'pug')
-app.set('views', 'views');
-const path = require('path');
+// Set up Handlebars view engine
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
+// Route imports
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminData.routes)
-app.use(shopRoutes)
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+// 404 handler
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
 
+// Start server
 app.listen(3000);
