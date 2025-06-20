fba34402fcd10aa86560bfd8ac94f7712ede07f9
Liciacodes <udosenfelicia@gmail.com>
2025-06-20 17:03:26 +0100

    feat: add Product model to store and retrieve product data from file

--- controllers/products.js
+++ controllers/products.js
@@ -12,18 +12,20 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
 const product = new Product(req.body.title)
-product.save();
+product.save()
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll()
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
+  Product.fetchAll((products)=> {
+    res.render("shop", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
+      });
+   })
+ 
 };
