85ed4bc3b38fe62df0e8a018ec5c8369d8f127cc
Liciacodes <udosenfelicia@gmail.com>
2025-07-01 23:19:28 +0100

    installed sequelize libray

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'udosenfelicia', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: 'udosenfelicia'
+  
 })
 
-module.exports = pool.promise()
+module.exports = sequelize;
