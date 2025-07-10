0e86a964ce9e864fe3e39ffea275523be703db92
Liciacodes <udosenfelicia@gmail.com>
2025-07-10 14:25:26 +0100

    Create new user, store user in db, work on cart items and orders, addingthe add to cart functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,6 @@
-
 const mongodb = require("mongodb");
 const Product = require("../models/product");
 // const { use } = require("../routes/admin");
-
-
-
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -19,11 +15,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  const product = new Product(title, price, imageUrl, description);
-   product
-   .save() 
-   .then(() => {
-    res.redirect("/admin/products");  
+  const product = new Product(
+    title,
+    price,
+    imageUrl,
+    description,
+    null,
+    req.user._id
+  );
+  product
+    .save()
+    .then(() => {
+      res.redirect("/admin/products");
     })
     .catch((err) => {
       console.log(err);
@@ -36,21 +39,20 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
- 
+
   Product.findById(prodId)
-  .then( product => {
-    
-    if (!product) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product: product,
-    });
-  })
-  .catch(err => console.log(err));
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -59,42 +61,44 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
- 
 
-  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl,  prodId);
-  
-   product.save()
-  .then( result => {
-    console.log("Updated Product");
-    res.redirect("/admin/products");
-  })
-  .catch(err => {
-    console.log(err);
-  })
- 
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+
+  product
+    .save()
+    .then((result) => {
+      console.log("Updated Product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
-Product.fetchAll()
-  .then(products => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
+  Product.fetchAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  })
-  .catch( err => {
-    console.log(err);
-  })
-  
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId)
-    .then(() => {
-     
-    })
+    .then(() => {})
     .then((result) => {
       console.log("Destroyed Product");
       res.redirect("/admin/products");
--- models/user.js
+++ models/user.js
@@ -1,16 +1,48 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+const ObjectId = mongodb.ObjectId;
 
-const sequelize = require("../util/database");
+class User {
+  constructor(username, email, cart, id) {
+    this.username = username;
+    this.email = email;
+    this.cart = cart;
+    this._id = id;
+    // ? cart : {items: [], totalPrice: 0};
+  }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
-});
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id
+    // })
+
+    const updatedCart = { items: [{ productId: new ObjectId(String(product._id)), quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(String(this._id)) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(String(userId)) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = User;
