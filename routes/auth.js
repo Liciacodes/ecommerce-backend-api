455c3a9d0dd14f900ec186366ac8f43ece67ec74
Liciacodes <udosenfelicia@gmail.com>
2025-07-24 19:15:42 +0100

    Implement Authentication, add Sign in functionality

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,15 @@ const authController = require('../controllers/auth')
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin)
 
-router.post('/login', authController.postLogin)
+router.get('/login', authController.getLogin);
 
-router.post('/logout', authController.postLogout )
+router.get('/signup', authController.getSignup);
+
+router.post('/login', authController.postLogin);
+
+router.post('/signup', authController.postSignup);
+
+router.post('/logout', authController.postLogout);
 
 module.exports = router
