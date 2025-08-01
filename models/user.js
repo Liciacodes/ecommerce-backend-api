d71b5ea9b6d7858f5a326e4f67743c33af7f4a3f
Liciacodes <udosenfelicia@gmail.com>
2025-08-01 16:58:00 +0100

    removed the api keys

--- models/user.js
+++ models/user.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
- 
   email: {
     type: String,
     required: true
   },
   password: {
-type: String,
-required: true
+    type: String,
+    required: true
   },
+  resetToken: String,
+  resetTokenExpiration: Date,
   cart: {
     items: [
       {
